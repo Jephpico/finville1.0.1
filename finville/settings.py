1737b9c28b3b70548352f922398b0cf01c089bf3
jephpico <amadasun.jephthah@gmail.com>
2022-10-04 20:45:50 +0100

    email

--- finville/settings.py
+++ finville/settings.py
@@ -232,7 +232,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = "devteam0x0@gmail.com"
-EMAIL_HOST_PASSWORD = "nbbeltmoomsarffp"
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
